9e806d36f65f3d7150058591f1d41540ca8430a3
hugoalle <hugoallemand41@hotmail.com>
2022-08-23 22:42:25 +0200

    couleur model reparee, toujours besoin de gerer la taille de la div entourante

--- main.js
+++ main.js
@@ -8,26 +8,42 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.getElementById('webgl')
 
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer(
   {
     canvas: canvas,
-    alpha: true
+    alpha: true,
+    antialias: true
   });
 renderer.setClearColor(0x000000, 0)
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 
 // Object in the scene
+
 // const geometry = new THREE.BoxGeometry(1, 1, 1);
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // const cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 // cube.position.x = 5;
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+scene.add(directionalLight)
+directionalLight.position.set(0, 5, 10)
+directionalLight.lookAt(0, 0, 0)
 
 let colonnesMesh = null
 const loader = new GLTFLoader()
@@ -40,14 +56,29 @@ loader.load(
   function (gltf) {
 
     colonnesMesh = gltf.scene
-    colonnesMesh.position.x = 5
+    // colonnesMesh.position.x = 5
     scene.add(colonnesMesh);
+    window.test = gltf
   }
 );
 
-camera.position.z = 5;
+camera.position.z = 4;
 controls.update()
 
+//resizing
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+//Animate
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
